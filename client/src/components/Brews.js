7b71931e7f7eedb9e34eb6debcb3915a7d32fb0e
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-12 06:55:19 -0700

    video-29 Add Cart Markup in Brews Component

--- client/src/components/Brews.js
+++ client/src/components/Brews.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Strapi from 'strapi-sdk-javascript/build/main';
-import { Box, Heading, Text, Image, Card, Button } from 'gestalt';
+import { Box, Heading, Text, Image, Card, Button, Mask } from 'gestalt';
+import { Link } from 'react-router-dom';
 const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
 
 class Brews extends React.Component {
-  state = { brews: [], brand: '' };
+  state = { brews: [], brand: '', cartItems: [] };
 
   async componentDidMount() {
     // console.log(this.props.match.params.brandId);
@@ -39,7 +40,7 @@ class Brews extends React.Component {
   }
 
   render() {
-    const { brand, brews } = this.state;
+    const { brand, brews, cartItems } = this.state;
 
     return (
       <Box
@@ -47,6 +48,11 @@ class Brews extends React.Component {
         diplay='flex'
         justifyContent='center'
         alignItems='start'
+        dangerouslySetInlineStyle={{
+          __style: {
+            flexWrap: 'wrap-reverse',
+          },
+        }}
       >
         {/* Brews Section */}
         <Box display='flex' direction='column' alignItems='center'>
@@ -103,6 +109,45 @@ class Brews extends React.Component {
               </Box>
             ))}
           </Box>
+        </Box>
+
+        {/* User Cart */}
+        <Box alignSelf='end' marginTop={2} marginLeft={8}>
+          <Mask shape='rounded' wash>
+            <Box
+              display='flex'
+              direction='column'
+              alignItems='center'
+              padding={2}
+            >
+              {/* User Cart Heading */}
+              <Heading align='center' size='md'>
+                Your Cart
+              </Heading>
+              <Text color='gray' italic>
+                {cartItems.length} items selected
+              </Text>
+
+              {/* Cart Items (will add) */}
+
+              <Box
+                display='flex'
+                alignItems='center'
+                justifyContent='center'
+                direction='column'
+              >
+                <Box margin={2}>
+                  {cartItems.length === 0 && (
+                    <Text color='red'>Please select some items</Text>
+                  )}
+                </Box>
+                <Text size='lg'>Total:$3.99</Text>
+                <Text>
+                  <Link to='/checkout'>Checkout</Link>
+                </Text>
+              </Box>
+            </Box>
+          </Mask>
         </Box>
       </Box>
     );
