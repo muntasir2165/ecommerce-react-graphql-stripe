f780879f1e8b41d768cc749328b97f38acfb018b
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-12 04:27:04 -0700

    video-27 Request Brews by Brand Id in Brews Component

---
+++ client/src/components/Brews.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Strapi from 'strapi-sdk-javascript/build/main';
+const apiUrl = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiUrl);
+
+class Brews extends React.Component {
+  async componentDidMount() {
+    // console.log(this.props.match.params.brandId);
+    try {
+      const response = await strapi.request('POST', '/graphql', {
+        data: {
+          query: `query {
+          brand(id: "${this.props.match.params.brandId}") {
+            _id
+            name
+            brews {
+              _id
+              name
+              image {
+                url
+              }
+              price
+            }
+          }
+        }`,
+        },
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    return <div>Brews</div>;
+  }
+}
+
+export default Brews;
