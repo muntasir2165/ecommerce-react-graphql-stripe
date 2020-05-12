36e4eba2196dc4ff639ba27abfc279d28260571d
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-12 07:38:37 -0700

    video-30 Create Function to Add Items to Cart

--- client/src/components/Brews.js
+++ client/src/components/Brews.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Strapi from 'strapi-sdk-javascript/build/main';
-import { Box, Heading, Text, Image, Card, Button, Mask } from 'gestalt';
+import {
+  Box,
+  Heading,
+  Text,
+  Image,
+  Card,
+  Button,
+  Mask,
+  IconButton,
+} from 'gestalt';
 import { Link } from 'react-router-dom';
 const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
@@ -39,13 +48,31 @@ class Brews extends React.Component {
     }
   }
 
+  addToCart = (brew) => {
+    const alreadyInCart = this.state.cartItems.findIndex(
+      (item) => item._id === brew._id
+    );
+
+    if (alreadyInCart === -1) {
+      const updatedItems = this.state.cartItems.concat({
+        ...brew,
+        quantity: 1,
+      });
+      this.setState({ cartItems: updatedItems });
+    } else {
+      const updatedItems = [...this.state.cartItems];
+      updatedItems[alreadyInCart].quantity += 1;
+      this.setState({ cartItems: updatedItems });
+    }
+  };
+
   render() {
     const { brand, brews, cartItems } = this.state;
 
     return (
       <Box
         marginTop={4}
-        diplay='flex'
+        display='flex'
         justifyContent='center'
         alignItems='start'
         dangerouslySetInlineStyle={{
@@ -101,7 +128,11 @@ class Brews extends React.Component {
                     <Text color='orchid'>${brew.price}</Text>
                     <Box marginTop={2}>
                       <Text bold size='xl'>
-                        <Button color='blue' text='Add to Cart' />
+                        <Button
+                          onClick={() => this.addToCart(brew)}
+                          color='blue'
+                          text='Add to Cart'
+                        />
                       </Text>
                     </Box>
                   </Box>
@@ -128,7 +159,21 @@ class Brews extends React.Component {
                 {cartItems.length} items selected
               </Text>
 
-              {/* Cart Items (will add) */}
+              {/* Cart Items */}
+              {cartItems.map((item) => (
+                <Box key={item._id} display='flex' alignItems='center'>
+                  <Text>
+                    {item.name} x {item.quantity} - $
+                    {(item.quantity * item.price).toFixed(2)}
+                  </Text>
+                  <IconButton
+                    accessibilityLabel='Delete Item'
+                    icon='cancel'
+                    size='sm'
+                    iconColor='red'
+                  />
+                </Box>
+              ))}
 
               <Box
                 display='flex'
